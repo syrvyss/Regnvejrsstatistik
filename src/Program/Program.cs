acb67c5202bcb3de5a8af5ffde4ac1df200a4ff9
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-19 12:21:12 +0200

    Initialized user data to double array

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Regnvejrsstatistik {
-    class Program {
-        public static Main() {
+﻿using System;
+using System.Linq;
 
+namespace Regnvejrsstatistik {
+    class Program {
+        public static void Main() {
+            Console.Write("How many data values to initialize? (usize): ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter {0} Rain downpour datavalues (double+space+double): ", size);
+            double[] data = new double[size];
+            Console.WriteLine(data.Length);
+
+            string[] a = Console.ReadLine().Split(" ");
+            if (a.Length != size)
+                Console.WriteLine("Your data got cut off: size is {0} but you typed in {1} values!", size, a.Length);
+
+            for (int i = 0; i < size; i++) {
+                data[i] = double.Parse(a[i]);
+            }
         }
     }
 }
