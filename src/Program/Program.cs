1703b660d74ddd544ee7fc4a55e10ae52125f5aa
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-19 11:19:20 +0200

    Added program structure

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Regnvejrsstatistik {
+    class Program {
+        public static Main() {
+
+        }
+    }
+}
