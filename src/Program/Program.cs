a3c0159c72dde2d8275fb5eab3cead73791fdf02
syrvyss <69795518+syrvyss@users.noreply.github.com>
2022-05-20 08:31:47 +0200

    Added Median method and called in menuOption (#4)

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Regnvejrsstatistik {
     class Program {
@@ -14,6 +15,7 @@ namespace Regnvejrsstatistik {
             string[] a = Console.ReadLine().Split(" ");
             if (a.Length != size) {
                 Console.WriteLine("Your data got cut off: size is {0} but you typed in {1} values! Exiting program.", size, a.Length);
+                Console.Clear();
                 System.Environment.Exit(1);
             }
 
@@ -24,13 +26,13 @@ namespace Regnvejrsstatistik {
             Statistics stat1 = new Statistics(data);
             
             while (true) {
-                Console.Write("Write out (Average: 0), (Min: 1), (Max: 2), (Values: 3), (Exit: 4): ");
+                Console.Write("Write out (Average: 0), (Min: 1), (Max: 2), (Median: 3), (Values: 4), (Exit: 5): ");
                 int menuOption = int.Parse(Console.ReadLine());
 
-                if (menuOption == 4)
+                if (menuOption == 5)
                     System.Environment.Exit(1);
 
-                if (menuOption == 3) {
+                if (menuOption == 4) {
                     stat1.Print();
                     continue;
                 }
@@ -40,6 +42,7 @@ namespace Regnvejrsstatistik {
                         0 => stat1.Average(),
                         1 => stat1.Min(),
                         2 => stat1.Max(),
+                        3 => stat1.Median(),
                         _ => throw new Exception()
                     };
 
@@ -63,11 +66,18 @@ namespace Regnvejrsstatistik {
         public double Average() => this._values.Average();
         public double Min() => this._values.Min();
         public double Max() => this._values.Max();
+        public double Median() {
+            double[] a = this._values;
+            Array.Sort(a);
+
+            if (a.Length % 2 == 0)
+                return (a[a.Length / 2 - 1] + a[a.Length / 2]) / 2;
+            else
+                return a[a.Length / 2];
+        } 
         public void Print() {
-            for (int i = 0; i < this._values.Length; i++)
-            {
+            for (int i = 0; i < this._values.Count(); i++)
                 Console.WriteLine("Value num: {0} \t{1} ", i+1, this._values[i]); // deleting the string formatting messes up the program somehow??
-            }
             Console.WriteLine("");
         }
     }
